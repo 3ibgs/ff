93f193d319d65211fc10aea84e8dfcbc709e155a
3ibgs <36799370+3ibgs@users.noreply.github.com>
2020-11-09 22:29:42 +0700

    Add files via upload

---
+++ bagiverff.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+d=`date +%m-%d-%Y`
+echo -e "\033[32m\e[1m      UPDATE SCRIP $d "
+echo -e "\033[35m\e[1m====================================\033[0m"
+echo -e "\033[35m\e[1m=== \033[35mBAGI - BAGI AKUN FREE FIRE \033[35m===\033[0m"
+echo -e "\033[35m\e[1m====================================\033[0m"
+sleep 1
+echo ""
+echo -e "\033[35m\e[1m\033[33m Youtube   : BAGIVER\033[35m\033[0"
+echo -e "\033[35m\e[1m\033[33m Youtube   : BOSGIVER\033[35m\033[0"
+echo -e "\033[35m\e[1m\033[33m Youtube   : GASGIVER\033[35m\033[0"
+echo -e "\033[35m\e[1m\033[33m Youtube   : BOYGIVER\033[35m\033[0"
+echo -e "\033[35m\e[1m\033[33m Youtube   : KANGGIVER\033[35m\033[0"
+echo -e "\033[35m\e[1m\033[33m Instagram : @3iBGS\033[35m\033[0"
+echo ""
+echo -e "\033[35m\e[1m\033[31m DILARANG KERAS!!!"
+echo -e "\033[35m\e[1m\033[31m 1. Menyalahgunakan Scrip"
+echo -e "\033[35m\e[1m\033[31m 2. Memperjualkan Scrip"
+echo -e "\033[35m\e[1m\033[31m 3. Membongkar Scrip"
+echo -e "\033[35m\e[1m\033[31m 4. Mereupload Scrip tanpa tanpa izin @3iBGS"
+echo ""
+echo -e "\033[32m\e[1m Jika Scrip Terjadi Kesalahan"
+echo -e "\033[32m\e[1m Silahkan DM Instagram Gua @3iBGS"
+echo -e "\033[32m\e[1m Akan Gua bantu secara GRATIS"
+echo -e "\033[33m\e[1m=================================\033[0m"
+sleep 1
+echo -e "\033[35m\e[1m  1. Bagi-bagi akun Free Fire\033[0m"
+echo -e "\033[35m\e[1m  2. Kode Redeem V.1\033[0m"
+echo -e "\033[35m\e[1m  3. Kode Redeem V.2\033[0m"
+echo -e "\033[35m\e[1m  4. Kode Redeem V.3\033[0m"
+echo -e "\033[35m\e[1m  5. Kode Redeem V.4\033[0m"
+echo -e "\033[35m\e[1m  6. Kode Redeem V.5\033[0m"
+echo -e "\033[35m\e[1m  7. Kode Redeem V.6\033[0m"
+echo -e "\033[35m\e[1m  8. Kode Redeem V.7\033[0m"
+echo -e "\033[35m\e[1m  9. Kode Redeem V.8\033[0m"
+echo ""
+echo -n "Pilih salah satu:"
+read pwd;
+if [ $pwd = "1" ]
+ then
+   sleep 1
+   echo ""
+   echo -e "\033[31m\e[1mTunggu sebentar..."
+   sleep 10
+   echo ""
+   echo -e "\033[32m\e[1mSelamat anda berhasil"
+   echo -e "\033[32m\e[1mAkun sedang disiapkan mohon tunggu sebentar..."
+   sleep 10
+   echo -e "\033[35m\e[1m==========================================\033[0m"
+   echo -e "\033[35m\e[1m=== \033[32m\e[1m> Login    :Facebook"
+   echo -e "\033[35m\e[1m=== \033[32m\e[1m> Email    :artisduniamaya@gmail.com"
+   echo -e "\033[35m\e[1m=== \033[32m\e[1m> Pw       :hanindia15"
+   echo -e "\033[35m\e[1m==========================================\033[0m"
+ else
+   sleep 3
+   echo -e "\033[35m\e[1m\033[31m Unlock Fitur Ini Harus Follow Instagram @3iBGS"
+   sleep 1
+   echo -e "\033[35m\e[1m\033[31m Unlock Fitur Ini Harus Subscribe BAGIVER"
+   sleep 1
+   echo -e "\033[35m\e[1m\033[31m Unlock Fitur Ini Harus Subscribe BOSGIVER"
+   sleep 1
+   echo -e "\033[35m\e[1m\033[31m Unlock Fitur Ini Harus Subscribe GASIVER"
+   sleep 1
+   echo -e "\033[35m\e[1m\033[31m Unlock Fitur Ini Harus Subscribe BOYIVER"
+   sleep 1
+   echo -e "\033[35m\e[1m\033[31m Unlock Fitur Ini Harus Subscribe KANGGIVER"
+   sleep 10000000000000
+fi
+    echo "Terimakasih..."
+
+
+
+
+
+
+
+
+
+
+
